7b4fbe46ac98c99e3df5c54bc533ca25d10c32d3
natootoki <ikoan0226@gmail.com>
2023-05-22 21:26:37 +0900

    x, y

---
+++ first.py
@@ -0,0 +1,23 @@
+#print
+print("Hello, world!")
+
+#変数の宣言、参照
+hoge = 1
+print(hoge)
+
+#if文
+if hoge:
+    print("hoge=true")
+
+#for文
+for i in range(10):
+    print(i)
+
+#各種演算
+print(1+1)
+print(2-1)
+print(2*3)
+print(5/2)
+print(33%7)
+print(7//2)
+print(7**2)
---
+++ test.py
@@ -0,0 +1,99 @@
+import os
+import time
+import keyboard
+import pynput
+from pynput import mouse, keyboard
+
+button = {}
+button["q"] = False
+
+global_x = 0
+global_y = 0
+
+# sample1.py
+def move(x, y):
+    print('マウスポインターは {0} へ移動しました'.format((x, y)))
+
+def click(x, y, button, pressed):
+    print('{2} が {0} された座標： {1}'.format(
+        'Pressed' if pressed else 'Released',(x, y), button))
+    if not pressed:     # クリックを離したら
+        return False    # Listenerを止める
+
+def scroll(x, y, dx, dy):
+    print('{0} スクロールされた座標： {1}'.format(
+        'down' if dy < 0 else 'up',(x, y)))
+
+mouse_listener = mouse.Listener(
+    on_move=move,
+    on_click=click,
+    on_scroll=scroll)
+#mouse_listener.start()
+
+# sample2.py
+def press(key):
+    global global_x
+    global global_y
+    try:
+        #print('アルファベット {0} が押されました'.format(key.char))
+        if "{0}".format(key.char) == "a":
+            global_x += 1
+        print(global_x, global_y)
+    except AttributeError:
+        if "{0}".format(key) == "Key.up":
+            global_y += 1
+        elif "{0}".format(key) == "Key.down":
+            global_y -= 1
+        elif "{0}".format(key) == "Key.right":
+            global_x += 1
+        elif "{0}".format(key) == "Key.left":
+            global_x -= 1
+        os.system('cls')
+        print(global_x, global_y)
+
+def release(key):
+    #print('{0} が離されました'.format(key))
+    if key == keyboard.Key.esc:     # escが押された場合
+        return False    # listenerを止める
+
+listener = keyboard.Listener(
+    on_press=press,
+    on_release=release)
+listener.start()
+
+while True:
+    time.sleep(1)
+
+os.system('cls')
+
+# time.sleep(1)
+
+# os.system('cls')
+# print("□■□□")
+# print("□□□□")
+# print("□□□□")
+# print("□□□□")
+# time.sleep(1)
+
+# os.system('cls')
+# print("□□□□")
+# print("□■□□")
+# print("□□□□")
+# print("□□□□")
+# time.sleep(1)
+
+# os.system('cls')
+# print("□□□□")
+# print("□□□□")
+# print("□■□□")
+# print("□□□□")
+# time.sleep(1)
+
+# os.system('cls')
+# print("□□□□")
+# print("□□□□")
+# print("□□□□")
+# print("□■□□")
+# time.sleep(1)
+
+# os.system('cls')
