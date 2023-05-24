0cb6df14bdc3f773135288eedcc59e6818c9f6fc
natootoki <ikoan0226@gmail.com>
2023-05-24 21:05:55 +0900

    nagaoshi

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ from pynput import mouse, keyboard
 
 button = {}
 button["q"] = False
+button["up"] = False
+button["down"] = False
+button["right"] = False
+button["left"] = False
 
 global_x = 0
 global_y = 0
@@ -34,6 +38,7 @@ mouse_listener = mouse.Listener(
 def press(key):
     global global_x
     global global_y
+    global button
     try:
         #print('アルファベット {0} が押されました'.format(key.char))
         if "{0}".format(key.char) == "a":
@@ -41,20 +46,34 @@ def press(key):
         print(global_x, global_y)
     except AttributeError:
         if "{0}".format(key) == "Key.up":
-            global_y += 1
-        elif "{0}".format(key) == "Key.down":
-            global_y -= 1
-        elif "{0}".format(key) == "Key.right":
-            global_x += 1
-        elif "{0}".format(key) == "Key.left":
-            global_x -= 1
-        os.system('cls')
-        print(global_x, global_y)
+            button["up"] = True
+        if "{0}".format(key) == "Key.down":
+            button["down"] = True
+        if "{0}".format(key) == "Key.right":
+            button["right"] = True
+        if "{0}".format(key) == "Key.left":
+            button["left"] = True
 
 def release(key):
-    #print('{0} が離されました'.format(key))
-    if key == keyboard.Key.esc:     # escが押された場合
-        return False    # listenerを止める
+    # print('{0} が離されました'.format(key))
+    # if key == keyboard.Key.esc:     # escが押された場合
+    #     return False    # listenerを止める
+    global button
+    try:
+        #print('アルファベット {0} が押されました'.format(key.char))
+        if "{0}".format(key.char) == "a":
+            global_x += 1
+        print(global_x, global_y)
+    except AttributeError:
+        if "{0}".format(key) == "Key.up":
+            button["up"] = False
+        if "{0}".format(key) == "Key.down":
+            button["down"] = False
+        if "{0}".format(key) == "Key.right":
+            button["right"] = False
+        if "{0}".format(key) == "Key.left":
+            button["left"] = False
+
 
 listener = keyboard.Listener(
     on_press=press,
@@ -62,7 +81,18 @@ listener = keyboard.Listener(
 listener.start()
 
 while True:
-    time.sleep(1)
+    if button["up"]:
+        global_y += 1
+    elif button["down"]:
+        global_y -= 1
+    elif button["right"]:
+        global_x += 1
+    elif button["left"]:
+        global_x -= 1
+    os.system('cls')
+    print(global_x, global_y)
+    time.sleep(0.05)
+    
 
 os.system('cls')
 
