20f565281c14ebfa3f12cf2033740cf3f28aeab8
natootoki <ikoan0226@gmail.com>
2023-06-08 07:31:10 +0900

    pygame move

--- screen.py
+++ screen.py
@@ -1,21 +1,42 @@
-import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import *
+import sys, random
 
-pygame.init()
-SURFACE = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("Just Window")
+# 変数の初期化
+maze_w = 31 # 迷路の列数
+maze_h = 23 # 迷路の行数
+maze = [] # 迷路データ
+tile_w = 16
+px = 1 # プレイヤーの座標
+py = 1
+
+# 色を定義
+black = (0, 0, 0)
+red = (255, 0, 0)
 
 def main():
-    while True:
-        SURFACE.fill(255, 255, 255)
+    # ゲームの初期化処理
+    global px, py
+    pygame.init()
+    pygame.display.set_caption("maze game")
+    screen = pygame.display.set_mode((tile_w*maze_w,tile_w*maze_h))
 
+    # ゲームのメインループ --- (*3)
+    while True:
+        # 背景
+        screen.fill(black)
+        # 長方形
+        pygame.draw.rect(screen, red, (0, 0, 32, 32))
+        # 円
+        pygame.draw.circle(screen, red, (32, 32), 16)
+        # 描画
+        pygame.display.update()
+        # イベントを処理する --- (*5)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        
-        pygame.display.update()
-
-    if __name__ =='__main__':
-        main()
+            if event.type == KEYDOWN:
+                print("hello!")
+if __name__ == '__main__':
+    main()
